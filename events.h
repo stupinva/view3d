7a5d4deeb427b6cb309572f63be2d1fce6d561c4
Vladimir Stupin <vladimir@stupin.su>
2018-11-16 23:23:17 +0500

    Отделение функций просмотра от главного модуля

---
+++ events.h
@@ -0,0 +1,21 @@
+#ifndef __EVENTS__
+#define __EVENTS__
+
+void Draw(void);
+void SwitchDrawSkin(void);
+void SwitchLighting(void);
+void SwitchDrawNormals(void);
+void DistUp(void);
+void DistDown(void);
+void PrevFrame(void);
+void NextFrame(void);
+void PrevSkin(void);
+void NextSkin(void);
+void YawUp(void);
+void YawDown(void);
+void AngleRight(void);
+void AngleLeft(void);
+int Init(const char *filename);
+void Reshape(unsigned width, unsigned height);
+
+#endif
